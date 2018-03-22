7a09391f5a8fa077e50212342a398e5f848403a4
Dan Shrader <Dan@Shraderland.com>
2018-03-21 21:07:25 -0400

    Little tweak to resolve the initial page load uglyness

--- gisty.js
+++ gisty.js
@@ -902,6 +902,7 @@ $(document).ready(function() {
     } else {
       localStorage.setItem("gistyTheme", "dark");
     }
+    $('.sidebar-wrapper, .language-wrapper').show()
     $("#page-content-wrapper").css('margin-left','550px');
   	app();
   }
